98096e7721e30e25441915225639545888eab692
jaderrson pessoa de macedo <jaderson.chip7@gmail.com>
2022-02-01 17:37:00 -0300

    .

--- src/controllers/hooks_controller.py
+++ src/controllers/hooks_controller.py
@@ -1,4 +1,5 @@
 import json
+from operator import index
 from src.db.hooks_data import HooksData
 from src.tools.utils import Formats
 
@@ -7,42 +8,8 @@ from src.server.instance import server
 
 
 app, api = server.app, server.api
-ns = api.namespace('Quality Assurance - Automation', path='/api/v1/automation/hooks')
-
-@ns.route('/<string:tag>')
-class RouteHooksByTag(Resource):
-    @ns.doc(
-        responses={
-            200: 'Sucess',
-            404: 'Failed to found config',
-            500: 'Internal Error'
-        },
-        description='feature for search configuration the of automations')
-    def get(self, tag):
-        repo = HooksData()
-        resp = Formats()
-        
-        try:
-            if not isinstance(tag, str):
-                return resp.formatResponse(400, 'Tag must be an String value ', '{}')
-
-            r = repo.selectByTag(tag)
-            msg = r['return']
-
-            if len(r['obj']) > 0:
-                d = []
-               
-                for item in r['obj']:
-                    d.append(item[0])
-
-                return resp.formatResponse(200, msg,  json.dumps(d, indent=4))
-
-            if len(r['obj']) == 0:
-                return resp.formatResponse(404, 'Not found Config', '{}')
-
-        except Exception as e:
-            if e.find('JSONDecodeError') >= 0:
-                return resp.formatResponse(500, e.msg, '{}')
+ns = api.namespace('Quality Assurance - Automation',
+                   path='/api/v1/automation/hooks')
 
 
 @ns.route('/')
@@ -57,24 +24,14 @@ class RouteHooks(Resource):
     def get(self):
         repo = HooksData()
         resp = Formats()
-        
+
         try:
             r = repo.selectAll()
             msg = r['return']
 
             if len(r['obj']) > 0:
-                d = {}
-
-                for item in r['obj']:
-                    a = item[0]
-                    b = item[1]
-
-                    if a in d.keys():
-                         print()
-                    else:
-                        d.update({a:b})
-
-                return resp.formatResponse(200, msg,  json.dumps(d, indent=4))
+                l = resp.formatResponseContract(r)
+                return resp.formatResponse(200, msg,  json.dumps(l, indent=4))
 
             if len(r['obj']) == 0:
                 return resp.formatResponse(404, 'Not found Config', '{}')
@@ -82,4 +39,3 @@ class RouteHooks(Resource):
         except Exception as e:
             if e.find('JSONDecodeError') >= 0:
                 return resp.formatResponse(500, e.msg, '{}')
-
--- src/tools/utils.py
+++ src/tools/utils.py
@@ -24,3 +24,51 @@ class Formats():
             return resp.formatResponse(400, err, payload)
         if err.find('but expression is of type') >= 0:
             return resp.formatResponse(400, err, payload)
+
+    def formatResponseContract(self, r):
+        l = {}
+        la = []
+
+        index = 0
+        i = len(r['obj']) - 1
+
+        for item in r['obj']:
+            k = item[0]
+            nk = ''
+
+            if index < i:
+                nk = r['obj'][index + 1][0]
+
+            if index == 0:
+                if k == nk:
+                    la.append(item[1])
+                else:
+                    v = item[1]
+                    l.update({k: v})
+
+            else:
+                pk = r['obj'][index - 1][0]
+
+                if k == nk:
+                    if len(la) > 0 and k == pk:
+                        la.append(item[1])
+
+                if k == pk:
+                    la.append(item[1])
+
+                if k != pk:
+                    v = item[1]
+                    
+                    if k != nk:
+                        l.update({k: v})
+
+                        if len(la) > 0:
+                            l.update({pk: la})
+                            la = []
+
+                if  index == i and len(la) > 0:
+                    l.update({k: la})  
+
+            index += 1
+            
+        return l
